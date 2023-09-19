2b923f93985796d2667299926d39b65966d43fdf
Manohar123261 <121283980+Manohar123261@users.noreply.github.com>
2023-09-19 17:19:12 +0530

    the login page is almost completed

---
+++ mptaas/index.js
@@ -0,0 +1,22 @@
+function createcap(){
+    // alert("undefined");
+    var capchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    var capchar = capchar.split("");
+    var a=capchar[Math.floor(Math.random()*74)];
+    var b=capchar[Math.floor(Math.random()*74)];
+    var c=capchar[Math.floor(Math.random()*74)];
+    var d=capchar[Math.floor(Math.random()*74)];
+    var e=capchar[Math.floor(Math.random()*74)];
+    var f=capchar[Math.floor(Math.random()*74)];
+    var captcha=a+b+c+d+e+f;
+    document.getElementById("cap").value=captcha;
+}
+function validatecap(){
+    if()
+    if(document.getElementById("cap").value==document.getElementById("inp-cap").value){
+        alert("form validation is successful.");
+    }
+    else{
+        alert("please enter valid captcha.");
+    }
+}
